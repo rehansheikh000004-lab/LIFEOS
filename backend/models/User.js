05c23563921f01480650c3ce2fd4df9583de6dec
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2026-01-03 12:08:21 +0530

    Add User model with schema definition

---
+++ backend/models/User.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  goal: { type: String, default: "" }
+});
+
+export default mongoose.models.User || mongoose.model("User", userSchema);
